fe67e902314970c168acb519b2b58d9534f6739e
Aaron Ouyang <a_ouyang@mail.fhsu.edu>
2021-05-09 20:52:58 -0400

    quotes 13

--- config/Database.php
+++ config/Database.php
@@ -1,24 +1,28 @@
 <?php
 class Database {
-    private static $host = 'y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';     
-    private static $username = 'dtlblzne3cfwivk8';
-    private static $db_name = 'gic4xphvg8gbi3j6';
-    private static $password = 'jozx23uetptqw1le';
-    private static $conn;
     
     //DB Connect
     public function connect(){
+        $url = getenv('JAWSDB_URL');
+        $dbparts = parse_url($url);
+    
+        $hostname = $dbparts['host'];
+        $username = $dbparts['username'];
+        $password = $dbparts['password'];
+        $database = ltrim($dbparts['path'],'/');
+            
         $this->conn = null;
 
         try {
-            $this->conn= new PDO('mysql:host=' . $this->host .';dbname=' . $this->db_name, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXECPTION);
+            $this->conn= new PDO("mysql:host=$hostname;         dbname=$database", $username, $password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
         } catch(PDOException $e){
             echo 'Connection Error ' . $e->getMessage();
         }
 
         return $this->conn;
     }
-
+}
     
 ?>
