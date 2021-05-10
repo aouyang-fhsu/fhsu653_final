bc211178d9e4062fd9980f3e7d37e818ccfa1aca
Aaron Ouyang <a_ouyang@mail.fhsu.edu>
2021-05-09 23:15:51 -0400

    quotes single 2

--- config/Database.php
+++ config/Database.php
@@ -1,28 +1,28 @@
 <?php
 class Database {
-    // private $host = 'localhost';
-    // private $db_name = 'quotesdb';
-    // private $username = 'root';
-    // private $password = '';
-    // private $conn;
+     private $host = 'localhost';
+     private $db_name = 'quotesdb';
+     private $username = 'root';
+     private $password = '';
+     private $conn;
     
     //DB Connect
     public function connect(){
-        $url = getenv('JAWSDB_URL');
-        $dbparts = parse_url($url);
+        // $url = getenv('JAWSDB_URL');
+        // $dbparts = parse_url($url);
         
-        $hostname = $dbparts['host'];
-        $username = $dbparts['user'];
-        $password = $dbparts['pass'];
-        $database = ltrim($dbparts['path'],'/');
+        // $hostname = $dbparts['host'];
+        // $username = $dbparts['user'];
+        // $password = $dbparts['pass'];
+        // $database = ltrim($dbparts['path'],'/');
 
-        $dsn = "mysql:host={$hostname};dbname={$database}";
+        // $dsn = "mysql:host={$hostname};dbname={$database}";
             
         $this->conn = null;
 
         try {
-            $this->conn = new PDO($dsn, $username, $password);
-            //$this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+            //$this->conn = new PDO($dsn, $username, $password);
+            $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             echo "Connected successfully";
         } catch(PDOException $e){
