6e472bf41c77fb098daa36f9f561eb96c7e4cc4a
Aaron Ouyang <a_ouyang@mail.fhsu.edu>
2021-05-09 21:24:22 -0400

    quotes 16

--- config/Database.php
+++ config/Database.php
@@ -5,11 +5,11 @@ class Database {
     public function connect(){
         $url = getenv('JAWSDB_URL');
         $dbparts = parse_url($url);
-    
+        
         $hostname = $dbparts['host'];
-        $username = $dbparts['username'];
-        $password = $dbparts['password'];
-        $database = $dbparts['Database'];
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        $database = ltrim($dbparts['path'],'/');
 
         $dsn = "mysql:host={$hostname};dbname={$database}";
             
