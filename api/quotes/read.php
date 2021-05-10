68dd20a30c39089bebc186c9f61e81dbc89ae06a
Aaron Ouyang <a_ouyang@mail.fhsu.edu>
2021-05-09 20:00:04 -0400

    quotes 3

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -3,8 +3,8 @@
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
 
-    require('../../config/Database.php')
-    require('../../model/Quotes.php')
+    require('../../config/Database.php');
+    require('../../model/Quotes.php');
 
     // Instantiante DB & Connect
     $database = new Database();
